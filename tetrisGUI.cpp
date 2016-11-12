a83ac6f3bbe35a58a58e649213dfaeaa38c50b21
BoyDun <bodun@stanford.edu>
2016-11-12 06:34:14 -0800

    Update tetrisGUI.cpp

--- tetrisGUI.cpp
+++ tetrisGUI.cpp
@@ -1,18 +1,41 @@
 
-#include <cctype>
-#include <cmath>
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <string>
-#include "console.h"
-#include "filelib.h"
-#include "grid.h"
+#include "tetrisGUI.h"
 #include "gwindow.h"
-#include "simpio.h"
+#include "vector.h"
 using namespace std;
 
-int main() {
+namespace BoggleGUI {
 
-    return 0;
+    static const int WINDOW_WIDTH = 500;
+    static const int WINDOW_HEIGHT = 500;
+    static const int numSideBlocksX = 5;
+    static const int numSideBlocksY = 5;
+    static const int BOARD_COLOR = 0xADD8E6;
+
+    static GWindow* window = NULL;
+
+    void initialize(){
+    	window = new GWindow(WINDOW_WIDTH, WINDOW_HEIGHT);
+    	window->setWindowTitle("3D Tetris");
+    	window->setExitOnClose(true);
+    	window->setColor(BOARD_COLOR);
+    	int blockWidth = WINDOW_WIDTH / numSideBlocksX;
+    	int blockHeight = WINDOW_HEIGHT / numSideBlocksY;
+    	for(int i = 1; i < numSideBlocksX; i++){
+    		window->drawLine(i * blockWidth, 0, i * blockWidth, window->getHeight());
+    	}
+    	for(int i = 1; i < numSideBlocksY; i++){
+    		window->drawLine(0, i * blockHeight, window->getWidth(), i * blockHeight);
+    	}
+    }
+
+    void displayBlock(int x, int y){
+
+    }
+
+    void updateColor(int x, int y){
+    	
+    }
+
 }
